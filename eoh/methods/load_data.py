17d574d36990a3eb8015cd283b62d474d247b520
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-12-05 18:19:32 +0800

    fix evaluation metric issue (custom metric for temasek dataset)

--- eoh/methods/load_data.py
+++ eoh/methods/load_data.py
@@ -1,36 +1,5 @@
 from .meta_prompt import MetaPrompt, PromptMode
 
-def prep_tf_node_v1(prompt_mode: bool = True):
-    """ 
-    Prepare test cases, meta prompt, and metric map for Temasek Foundation dataset
-    1st version, overly complicate with custom metric functionals 
-    """
-    
-    import sys 
-    sys.path.append("../tuning/") 
-    from optm.soft_prompt import load_tf_data, tf_metric_map, BASE_PATH
-    train_data, test_data = load_tf_data(BASE_PATH)
-    
-    mode = PromptMode.PROMPT if prompt_mode else PromptMode.CODE
-    
-    tf_meta_prompt = MetaPrompt(
-        task = "Evaluate grant application, make a decision (Yes, No, Maybe) and a brief comment explanating your decision on why this project is likely to be accepted or rejected.",
-        func_name = "evaluate_grant",
-        inputs = ["project_description"],
-        outputs = ["label", "comment"],
-        input_types = ["str"],
-        output_types = ["str", "str"],
-        mode = mode
-    )
-
-    # Prepare test cases :: input dict & output dict
-    test_cases = []
-    for prompt, label, comment in zip(train_data["prompt"], train_data["label"], train_data["comment"]):
-        test_cases.append(({"project_description": prompt}, {"label": label, "comment": comment}))
-    
-    return tf_meta_prompt, test_cases, tf_metric_map
-
-
 def prep_tf_node(prompt_mode: bool = True):
     """
     2nd ver. bool object for label (care about No and Non-No -- yes & maybe)
@@ -46,7 +15,7 @@ def prep_tf_node(prompt_mode: bool = True):
         task = "Evaluate grant application, make a decision (Yes, No, Maybe) and a brief comment explanating your decision on why this project is likely to be accepted or rejected.",
         func_name = "evaluate_grant",
         inputs = ["project_description"],
-        outputs = ["label", "comment"],
+        outputs = ["decision", "comment"],
         input_types = ["str"],
         output_types = ["bool", "str"],
         mode = mode
@@ -60,8 +29,31 @@ def prep_tf_node(prompt_mode: bool = True):
     
     test_cases = []
     for prompt, label, comment in zip(train_data["prompt"], train_data["label"], train_data["comment"]):
-        test_cases.append(({"project_description": prompt}, {"label": map_label(label), "comment": comment}))
+        test_cases.append(({"project_description": prompt}, {"decision": map_label(label), "comment": comment}))
         
     return tf_meta_prompt, test_cases
+
+
+# Custom Metric for Temasek Foundation dataset
+
+def map_decision_to_bool(decision: str) -> bool:
+    yes_list = ["yes", "approve", "accept", "approved", "true"]
+    no_list = ["no", "reject", "decline", "rejected", "false"]
+    if decision in yes_list:
+        return True
+    elif decision in no_list:
+        return False
+    else:
+        raise ValueError(f"Unknown decision: {decision}")
     
+def compare_decision(pred_decision: str, target_decision: bool) -> bool:
+    pred_decision = map_decision_to_bool(pred_decision.lower())
     
+    is_aligned = pred_decision == target_decision
+    if not is_aligned:
+        err_msg = f"Decision mismatch: {pred_decision} (pred) != {target_decision} (target)"
+    else:
+        err_msg = ""
+    return is_aligned, err_msg
+    
+custom_metric_map = {"decision": compare_decision}
